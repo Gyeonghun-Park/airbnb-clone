5c5b17ef55fcb345e5cebb352a91b7a0d65695d3
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-16 16:17:56 -0400

    seed_facilities and seed_users

--- rooms/management/commands/seed_amenities.py
+++ rooms/management/commands/seed_amenities.py
@@ -3,6 +3,9 @@ from rooms.models import Amenity
 
 
 class Command(BaseCommand):
+
+    help = "This command creates amenities"
+
     def handle(self, *args, **options):
         amenities = [
             "Air conditioning",
@@ -51,4 +54,4 @@ class Command(BaseCommand):
 
         for a in amenities:
             Amenity.objects.create(name=a)
-        self.stdout.write(self.style.SUCCESS("Amenities created!"))
+        self.stdout.write(self.style.SUCCESS(f"{len(amenities)} Amenities created!"))

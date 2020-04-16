8e625931c5a7e3f647dcb15c2a60043d1bced292
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-16 01:06:33 -0400

    raw_ids and Inline Admin

--- rooms/models.py
+++ rooms/models.py
@@ -94,6 +94,8 @@ class Room(core_models.TimeStampedModel):
 
     def total_rating(self):
         all_reviews = self.reviews.all()
+        if len(all_reviews) == 0:
+            return 0
         all_ratings = 0
         for review in all_reviews:
             all_ratings += review.rating_average()

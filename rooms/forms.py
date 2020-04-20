a2e6d6c7f3d1c16c4697701d62bc24cec5a2ca51
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-19 21:57:50 -0400

    Using forms.py to makes static template

--- rooms/forms.py
+++ rooms/forms.py
@@ -1,12 +1,32 @@
 from django import forms
+from django_countries.fields import CountryField
 from . import models
 
-# Making forms is like models.py
-# https://docs.djangoproject.com/en/2.2/ref/forms/
+# Making forms should be same as fieldsname as models.py
+# https://docs.djangoproject.com/en/2.2/ref/forms/api/
+# Field arguments such as default, required can be looked up here https://docs.djangoproject.com/en/2.2/ref/forms/fields/
 
 
 class SearchForm(forms.Form):
 
+    # Widget customization: https://docs.djangoproject.com/en/2.2/ref/forms/fields/#widget
     city = forms.CharField()
     price = forms.IntegerField(required=False)
-    room_type = forms.ModelChoiceField(queryset=models.RoomType.objects.all())
+    country = CountryField(default="KR").formfield()
+    # foreignkey field should connect to models.py with queryset of the class
+    room_type = forms.ModelChoiceField(
+        required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
+    )
+    price = forms.IntegerField(required=False)
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+    amenities = forms.ModelMultipleChoiceField(
+        queryset=models.Amenity.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
+    facilities = forms.ModelMultipleChoiceField(
+        queryset=models.Facility.objects.all(), widget=forms.CheckboxSelectMultiple
+    )

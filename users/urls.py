136acfaa60b33d95530b8694695bb0284e6f1563
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-21 00:33:58 -0400

    Log In and Log Out

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+# exporting user's url routing to django project
 app_name = "users"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+# get functions from user app's views.py, export them to templates as name="users"
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
+]

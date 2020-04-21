abbab683c496454fb36194e95e45dcd2478e07a5
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-21 00:51:31 -0400

    Log In and Log Out Using FormView

--- users/views.py
+++ users/views.py
@@ -1,32 +1,43 @@
 from django.views import View
+
+# https://ccbv.co.uk/projects/Django/3.0/django.views.generic.edit/FormView/
+from django.views.generic import FormView
+
+# reverse_lazy to prevent circular import
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 
 # https://docs.djangoproject.com/en/3.0/topics/auth/default/#authenticating-users
 from django.contrib.auth import authenticate, login, logout
+
+# import users app's login forms
 from . import forms
 
 # Create your views here.
 
 
-class LoginView(View):
-    def get(self, request):
-        form = forms.LoginForm(initial={"email": "something@whatever.com"})
-        return render(request, "users/login.html", {"form": form})
+class LoginView(FormView):
 
-    def post(self, request):
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            # cleaned data is the cleaned result of all fields
-            email = form.cleaned_data.get("email")
-            password = form.cleaned_data.get("password")
-            user = authenticate(request, username=email, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(reverse("core:home"))
-        return render(request, "users/login.html", {"form": form})
+    """ Login View """
+
+    # Using inherited FormView class instead of LoginView: https://ccbv.co.uk/projects/Django/3.0/django.views.generic.edit/FormView/
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
 
 
-# https://docs.djangoproject.com/en/3.0/topics/auth/default/#how-to-log-a-user-out
+# Logout function: https://docs.djangoproject.com/en/3.0/topics/auth/default/#how-to-log-a-user-out
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+# LogoutView class: https://docs.djangoproject.com/en/3.0/topics/auth/default/#django.contrib.auth.views.LogoutView

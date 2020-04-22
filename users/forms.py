80f2f83a45f79a7b0dca3d2cec2bca57c33a8114
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-21 23:50:34 -0400

    Signup Form Part Three

--- users/forms.py
+++ users/forms.py
@@ -8,66 +8,41 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
     def clean(self):
-        # get the email data that user sent to us, clean data
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-
-        # try to find matching email object in Users database
         try:
             user = models.User.objects.get(email=email)
-            # check_password():
-            # https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#django.contrib.auth.models.AbstractBaseUser.check_password
-            # check_password() is encrypting password, and if the encrypted password is
-            # saved on the database
             if user.check_password(password):
                 return self.cleaned_data
             else:
                 self.add_error("password", forms.ValidationError("Password is wrong"))
-        # if user's email object does not exist in db, then raise error
-        # raising error prevents reaching to views.py LoginView Post request
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.Form):
-    # providing form fields to views.py -> urls.py -> template
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
     password = forms.CharField(widget=forms.PasswordInput)
-    password_again = forms.CharField(
-        widget=forms.PasswordInput, label="Confirm Password"
-    )
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
-    def clean_email(self):
-        # get cleaned data from template
-        email = self.cleaned_data.get("email")
-        # if finds existing user in database, then raise error
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists")
-        # else, if it doesn't finds user in database, proceed sign up
-        except models.User.DoesNotExist:
-            # return email to database
-            return email
-
-    def clean_password_again(self):
-        # get cleaned data from template
+    def clean_password1(self):
         password = self.cleaned_data.get("password")
-        password_again = self.cleaned_data.get("password_again")
-        # if fetched password doesn't match password again then raise validation error.
-        if password != password_again:
+        password1 = self.cleaned_data.get("password1")
+        if password != password1:
             raise forms.ValidationError("Password confirmation does not match")
         else:
-            # return password to database
             return password
 
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
+    # ModelForm has save method: https://docs.djangoproject.com/en/3.0/topics/forms/modelforms/#the-save-method
+    def save(self, *args, **kwargs):
+        # Additional interception is taken place with commit=False: create object but don't put it in database
+        user = super().save(commit=False)
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-        user = models.User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
+        user.username = email
+        # hashing password with set_password: https://docs.djangoproject.com/en/3.0/ref/contrib/auth/#django.contrib.auth.models.User.set_password
+        user.set_password(password)
         user.save()

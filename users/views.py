3ceabbdb9d1f3107eca73ea218ab849ecf16de21
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-21 21:22:41 -0400

    Signup Form

--- users/views.py
+++ users/views.py
@@ -1,26 +1,11 @@
-from django.views import View
-
-# https://ccbv.co.uk/projects/Django/3.0/django.views.generic.edit/FormView/
 from django.views.generic import FormView
-
-# reverse_lazy to prevent circular import
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, reverse
-
-# https://docs.djangoproject.com/en/3.0/topics/auth/default/#authenticating-users
+from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-
-# import users app's login forms
 from . import forms
-
-# Create your views here.
 
 
 class LoginView(FormView):
-
-    """ Login View """
-
-    # Using inherited FormView class instead of LoginView: https://ccbv.co.uk/projects/Django/3.0/django.views.generic.edit/FormView/
     template_name = "users/login.html"
     form_class = forms.LoginForm
     success_url = reverse_lazy("core:home")
@@ -34,10 +19,17 @@ class LoginView(FormView):
         return super().form_valid(form)
 
 
-# Logout function: https://docs.djangoproject.com/en/3.0/topics/auth/default/#how-to-log-a-user-out
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
 
 
-# LogoutView class: https://docs.djangoproject.com/en/3.0/topics/auth/default/#django.contrib.auth.views.LogoutView
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "Gyeonghun",
+        "last_name": "Park",
+        "email": "gp112795@gmail.com",
+    }

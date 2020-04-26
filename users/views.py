2de208f0b2def65cfc3cb47bd3944464cd484bfc
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-25 22:49:24 -0400

    Change Password

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 
 import os
 import requests
+from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
@@ -159,3 +160,8 @@ class UpdateProfileView(UpdateView):
 
     def get_object(self, queryset=None):
         return self.request.user
+
+
+class UpdatePasswordView(PasswordChangeView):
+
+    template_name = "users/update-password.html"

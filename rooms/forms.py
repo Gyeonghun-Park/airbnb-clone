611b4a2edcb8be063466fa6ac2a8d69898881829
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-26 23:47:07 -0400

    Add Photo Part Two

--- rooms/forms.py
+++ rooms/forms.py
@@ -44,4 +44,6 @@ class CreatePhotoForm(forms.ModelForm):
 
     def save(self, pk, *args, **kwargs):
         photo = super().save(commit=False)
-        print(pk)
+        room = models.Room.objects.get(pk=pk)
+        photo.room = room
+        photo.save()

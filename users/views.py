813ca4559c6f1389f3ec8029398129fb37e40cf2
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-04-30 21:51:40 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 
 import os
 import requests
+from django.conf import settings
 from django.utils import translation
 from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
@@ -217,6 +218,7 @@ def switch_hosting(request):
 
 def switch_language(request):
     lang = request.GET.get("lang", None)
-    if lang is not None:
-        request.session[translation.LANGUAGE_SESSION_KEY] = lang
-    return HttpResponse(status=200)
+    translation.activate(lang)
+    response = HttpResponse(status=200)
+    response.set_cookie(settings.LANGUAGE_COOKIE_NAME, lang)
+    return response

44e2e6157f36968a5bdc76b1594b1133d24090a8
Gyeonghun-Park <52763483+Gyeonghun-Park@users.noreply.github.com>
2020-09-15 00:08:35 -0400

    Sentry

--- config/urls.py
+++ config/urls.py
@@ -19,10 +19,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
     path("rooms/", include("rooms.urls", namespace="rooms")),
-    path("admin/", admin.site.urls),
+    path("potato12/", admin.site.urls),
+    path("sentry-debug/", trigger_error),
     path("users/", include("users.urls", namespace="users")),
     path("reservations/", include("reservations.urls", namespace="reservations")),
     path("reviews/", include("reviews.urls", namespace="reviews")),
